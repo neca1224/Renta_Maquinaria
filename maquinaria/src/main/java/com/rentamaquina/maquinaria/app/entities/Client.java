a825b2001f15c34fd2ef9459c3f0843ce90bc85a
neca1224 <36201374+neca1224@users.noreply.github.com>
2021-10-21 20:49:42 -0500

    cambios en el orden de los atributos de Client

--- maquinaria/src/main/java/com/rentamaquina/maquinaria/app/entities/Client.java
+++ maquinaria/src/main/java/com/rentamaquina/maquinaria/app/entities/Client.java
@@ -32,10 +32,10 @@ public class Client implements Serializable {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer idClient;
     @Column(length=50)
-    private String name;
-    private Integer age;
     private String email;
     private String password;
+    private String name;
+    private Integer age;
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "client")
     @JsonIgnoreProperties("client")
     private List<Message> messages;
